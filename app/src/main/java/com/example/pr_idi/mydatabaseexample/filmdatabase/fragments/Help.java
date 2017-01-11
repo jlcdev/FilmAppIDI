b308309ec33c1666246fda0d87eee35378d0c549
Javier López Calderón <potnox@gmail.com>
2017-01-11 20:44:05 +0100

    change help text

--- app/src/main/java/com/example/pr_idi/mydatabaseexample/filmdatabase/fragments/Help.java
+++ app/src/main/java/com/example/pr_idi/mydatabaseexample/filmdatabase/fragments/Help.java
@@ -55,50 +55,50 @@ public class Help extends Fragment {
         TextView texto13 = (TextView) view.findViewById(R.id.textView20);
         texto1.setText("Vista principal y menú\n");
 
-        texto2.setText("El menú es desplegable pulsando sobre el icono de menú o arrastrando el dedo de forma horizontal " +
-                        "desde el borde izquierdo de la pantalla hacia el centro.\n\n" +
-                        "Disponemos de las siguientes opciones:\n");
+        texto2.setText("El menú es desplegable fent clic sobre l'icona de menú o arrossegant el dit de forma horitzontal " +
+                "des de la bora esquerra de la pantalla cap al centre.\n\n" +
+                "Disposem de les següents opcions:\n");
 
-        texto3.setText("Buscar por título\n" +
-                        "Buscar por actores / actrices\n" +
-                        "Listado de películas\n" +
-                        "Añadir película\n\n");
+        texto3.setText("Cercar per títol\n" +
+                "Buscar por actors / actrius\n" +
+                "Listat de pel·lícules\n" +
+                "Afegir pel·lícula\n\n");
 
-        texto4.setText("Buscar por título\n");
+        texto4.setText("Cercar per títol\n");
 
-        texto5.setText("Esta es la vista principal además de una opción en el menú\n\n" +
-                        "Nos mostrará una lista de películas por pantalla. \n\n" +
-                        "Las películas, estarán ordenadas por nombre en orden alfabético. \n\n" +
-                        "Junto a cada una de las papeleras nos aparecerá un icono de borrado. Pulsar sobre el botón de borrado lanzará un mensaje de confirmación de la acción. En caso afirmativo la película será borrada de forma definitiva de la aplicación.\n\n" +
-                        "Pulsar sobre el título de una película nos moverá a una vista donde podremos ver los detalles (lista de películas) de la información de la película seleccionada.\n\n");
-        texto6.setText("Buscar por actores / actrices\n");
+        texto5.setText("Aquesta és la vista principal a més d'una opció al menú\n\n" +
+                "Ens mostrarà una llista de pel·lícules per pantalla. \n\n" +
+                "Les pel·lícules, estaran ordenades per nom en ordre alfabètic. \n\n" +
+                "Just al costat de cada nom ens apareixerà una paperera com a icona d'esborrat. Fer clic sobre el botó d'esborrar llançarà un missatge de confirmació de l'acció. En cas afirmatiu la pel·lícula serà esborrada de forma definitiva de l'aplicació.\n\n" +
+                "Fer clic sobre el títol d'una pel·lícula ens mourà a una vista on podrem veure els detalls de la informació de la pel·lícula seleccionada.\n\n");
+        texto6.setText("Buscar per actors / actrius\n");
 
-        texto7.setText("Nos mostrará una lista de actores protagonistas de las películas almacenadas en la aplicación.\n\n" +
-                        "Los nombres de los actores estarán ordenados en orden alfabético.\n\n" +
-                        "Pulsar sobre uno de los actores nos mostrará una lista detallada (lista de películas) de las películas que ha protagonizado.\n\n");
+        texto7.setText("Ens mostrarà una llista d'actors protagonistes de les pel·lícules emmagatzemades a l'aplicació.\n\n" +
+                "Els noms dels actors estaran ordenats en ordre alfabètic.\n\n" +
+                "Fer clic sobre un dels actors ens mostrarà una llista detallada de les pel·lícules que ha protagonitzat.\n\n");
 
-        texto8.setText("Lista de películas\n");
+        texto8.setText("Llista de pel·lícules\n");
 
-        texto9.setText("Nos mostrará una lista detallada de todas las películas disponibles.\n" +
-                        "Mostrando\n");
+        texto9.setText("Ens mostrarà una llista detallada de totes les pel·lícules disponibles.\n" +
+                "Mostrant\n");
 
-        texto10.setText("Título\n" +
-                        "Año de estreno\n" +
-                        "País\n" +
-                        "Director/a\n" +
-                        "Protagonista\n" +
-                        "Notas de la crítica\n\n");
+        texto10.setText("Títol\n" +
+                "Any de estrena\n" +
+                "País\n" +
+                "Director/a\n" +
+                "Protagonista\n" +
+                "Notes de la crítica\n\n");
 
-        texto11.setText("Añadir película\n");
+        texto11.setText("Afegir pel·lícula\n");
 
-        texto12.setText("Nos mostrará un formulario donde introducir los datos de la nueva película a introducir en la aplicación.\n");
+        texto12.setText("Ens mostrarà un formulari on introduir les dades de la nova pel·lícula a introduir a l'aplicació.\n");
 
-        texto13.setText("Título: Campo de texto\n" +
-                        "Año de estreno: Menú desplegable desde 1985 hasta la actualidad\n" +
-                        "País: Menú desplegable ordenado alfabéticamente.\n" +
-                        "Director/a: Campo de texto\n" +
-                        "Protagonista: Campo de texto\n" +
-                        "Nota de la crítica: Habrá una caja con la nota asignada y unos botones a izquierda y derecha para modificarla.\n");
+        texto13.setText("Títol: Camp de text\n" +
+                "Any d'estrena: Menú desplegable des de 1985 fins a l'actualitat\n" +
+                "País: Menú desplegable ordenat alfabèticament.\n" +
+                "Director/a: Camp de text.\n" +
+                "Protagonista: Camp de text.\n" +
+                "Nota de la crítica: Hi haurà una capsa amb la nota assignada i una barra tàctil per modificar-la.\n");
         return view;
     }
 
